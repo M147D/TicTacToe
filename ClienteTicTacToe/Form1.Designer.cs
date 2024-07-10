807455d5d4a7e7f74110491b9f0a2d85ba94e1fc
M147D <david.rmig@gmail.com>
2024-07-10 08:57:36 -0500

    Creación de formulario para el cliente

---
+++ ClienteTicTacToe/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ClienteTicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnUnir = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Location = new System.Drawing.Point(12, 61);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(79, 63);
+            this.btn00.TabIndex = 0;
+            this.btn00.Text = "button1";
+            this.btn00.UseVisualStyleBackColor = true;
+            // 
+            // btn01
+            // 
+            this.btn01.Location = new System.Drawing.Point(111, 61);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(79, 63);
+            this.btn01.TabIndex = 1;
+            this.btn01.Text = "button2";
+            this.btn01.UseVisualStyleBackColor = true;
+            // 
+            // btn02
+            // 
+            this.btn02.Location = new System.Drawing.Point(208, 61);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(79, 63);
+            this.btn02.TabIndex = 2;
+            this.btn02.Text = "button3";
+            this.btn02.UseVisualStyleBackColor = true;
+            // 
+            // btn12
+            // 
+            this.btn12.Location = new System.Drawing.Point(208, 144);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(79, 63);
+            this.btn12.TabIndex = 5;
+            this.btn12.Text = "button6";
+            this.btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            this.btn11.Location = new System.Drawing.Point(111, 144);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(79, 63);
+            this.btn11.TabIndex = 4;
+            this.btn11.Text = "button5";
+            this.btn11.UseVisualStyleBackColor = true;
+            // 
+            // btn10
+            // 
+            this.btn10.Location = new System.Drawing.Point(12, 144);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(79, 63);
+            this.btn10.TabIndex = 3;
+            this.btn10.Text = "button4";
+            this.btn10.UseVisualStyleBackColor = true;
+            // 
+            // btn22
+            // 
+            this.btn22.Location = new System.Drawing.Point(208, 225);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(79, 63);
+            this.btn22.TabIndex = 8;
+            this.btn22.Text = "button9";
+            this.btn22.UseVisualStyleBackColor = true;
+            // 
+            // btn21
+            // 
+            this.btn21.Location = new System.Drawing.Point(111, 225);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(79, 63);
+            this.btn21.TabIndex = 7;
+            this.btn21.Text = "button8";
+            this.btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn20
+            // 
+            this.btn20.Location = new System.Drawing.Point(12, 225);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(79, 63);
+            this.btn20.TabIndex = 6;
+            this.btn20.Text = "button7";
+            this.btn20.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(30, 318);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 9;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnUnir
+            // 
+            this.btnUnir.Location = new System.Drawing.Point(192, 318);
+            this.btnUnir.Name = "btnUnir";
+            this.btnUnir.Size = new System.Drawing.Size(75, 23);
+            this.btnUnir.TabIndex = 10;
+            this.btnUnir.Text = "Unir";
+            this.btnUnir.UseVisualStyleBackColor = true;
+            this.btnUnir.Click += new System.EventHandler(this.btnUnir_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(131, 20);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 13);
+            this.lblEstado.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 359);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.btnUnir);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnUnir;
+        private System.Windows.Forms.Label lblEstado;
+    }
+}
+
--- TicTacToe/ServicioTicTacToe.svc.cs
+++ TicTacToe/ServicioTicTacToe.svc.cs
@@ -12,34 +12,47 @@ namespace TicTacToe
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioTicTacToe.svc o ServicioTicTacToe.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class ServicioTicTacToe : IServicioTicTacToe
     {
-        //
+        // Variable que representa el tablero con una matrix 3x3 
         private static char[,] tablero = new char[3, 3];
+        // Variable con el caracter que gana la partida
         private static char ganador = ' ';
+        // Variable que identifica que jugador tiene el turno
         private static int jugadorActual = 1;
-        private static Dictionary<int, char> jugadores = new
-        Dictionary<int, char>();
+        // Variable que almacena la informacion sobre los jugadores durante la partida
+        private static Dictionary<int, char> jugadores = new Dictionary<int, char>();
 
+        // Metodo que inicializa las variables para cada jugador
         public void EmpezarJuego()
         {
+            // Se inicializa el tamaño de la matrix en 3x3 de tipo char (caracter) para el jugador
             tablero = new char[3, 3];
+            // Se inicializa el caracter ganador con un espacio en blanco
             ganador = ' ';
+            // Se inicializa el turno del jugador, sería el primer turno
             jugadorActual = 1;
+            // Se limpia la información previa de jugadores en otras partidas
             jugadores.Clear();
         }
-
+        // Metodo para controlar y controlar los movimientos del jugador
         public bool Mover(int idJugador, int x, int y)
         {
-            if (!jugadores.ContainsKey(idJugador) || tablero[x,
-           y] != '\0' || ganador != ' ')
+            // Primero, se verifica que el jugador se encuentre en el diccinario; segundo,
+            // se verifica que el valor almacenado en el tablero no sea nulo; tercero,
+            // se verifica que ganador no este en blanco
+            if (!jugadores.ContainsKey(idJugador) || tablero[x,y] != '\0' || ganador != ' ')
+                // Se retorna falso en caso de que almenos 1 de las 3 condiciones se cumpla
                 return false;
-            if ((jugadorActual == 1 && jugadores[idJugador] ==
-           'X') || (jugadorActual == 2 && jugadores[idJugador] ==
-           'O'))
+            // Se verifica que uno de los jugadores de la partida esten identificados con 'X' o 'O'
+            // en el diccionario para registrar sus movimientos.
+            if ((jugadorActual == 1 && jugadores[idJugador] == 'X') || (jugadorActual == 2 && jugadores[idJugador] == 'O'))
             {
+                // Se asigna al jugador actual en la posisción (x,y)
                 tablero[x, y] = jugadores[idJugador];
+                // Aqui se alterna el turno de los jugadores, de forma que si es el Jugador 1, se cambia al Jugador 2 y viceversa
                 jugadorActual = jugadorActual == 1 ? 2 : 1;
+                // 
                 ganador = DeterminarGanador() == "Nadie" ? ' ' :
-               jugadores[idJugador];
+                jugadores[idJugador];
                 return true;
             }
             return false;
@@ -50,6 +63,37 @@ namespace TicTacToe
             return string.Join(",",
             tablero.Cast<char>().Select(c => c ==
            '\0' ? ' ' : c)); ;
+        }
+        public string DeterminarGanador()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (tablero[i, 0] == tablero[i, 1] && tablero[i,
+               1] == tablero[i, 2] && tablero[i, 0] != '\0')
+                    return tablero[i, 0].ToString();
+                if (tablero[0, i] == tablero[1, i] && tablero[1,
+               i] == tablero[2, i] && tablero[0, i] != '\0')
+                    return tablero[0, i].ToString();
+            }
+            if (tablero[0, 0] == tablero[1, 1] && tablero[1, 1]
+           == tablero[2, 2] && tablero[0, 0] != '\0')
+                return tablero[0, 0].ToString();
+            if (tablero[0, 2] == tablero[1, 1] && tablero[1, 1]
+           == tablero[2, 0] && tablero[0, 2] != '\0')
+                return tablero[0, 2].ToString();
+            foreach (char celda in tablero)
+                if (celda == '\0')
+                    return "Nadie";
+
+            return "Empate";
+        }
+        public int Unir()
+        {
+            if (jugadores.Count >= 2)
+                return -1;
+            int idJugador = jugadores.Count + 1;
+            jugadores.Add(idJugador, idJugador == 1 ? 'X' : 'O');
+            return idJugador;
         }
     }
 }
